968ce354da7fe562f2bd62a88652fe38c3d8ed66
翠花 <im@nan.ge>
2020-06-05 21:30:09 +0800

    Update PDF Expert.js

--- Surge/Script/PDF Expert.js
+++ Surge/Script/PDF Expert.js
@@ -5,7 +5,7 @@ PDF Expert Unlock
 hostname = license.pdfexpert.com
 
 [Script]
-Pdfexpert.js = type=http-response,pattern=^https?:\/\/license\.pdfexpert\.com\/api\/1\.0\/pdfexpert6\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/wubulaba/surgescript/master/Script/Pdfexpert.js,script-update-interval=0
+PDF Expert = type=http-response,pattern=^https?:\/\/license\.pdfexpert\.com\/api\/1\.0\/pdfexpert6\/subscription\/(refresh$|check$),requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/XOS/Profiles/Her/Surge/Script/PDF%20Expert.js,script-update-interval=0
 */
 
 var obj= {
