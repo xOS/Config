84d089a0a73049bfd45f16e8d784dced0c39bef5
翠花 <admin@nange.cn>
2020-11-28 21:17:46 +0800

    Create WeChat.js

---
+++ Surge/Script/WeChat.js
@@ -0,0 +1,14 @@
+/*
+[Script]
+http-request ^https://mp\.weixin\.qq\.com/mp/getappmsgad script-path=https://config.nange.cn/Config/Surge/Script/WeChat.js
+[MITM]
+hostname = mp.weixin.qq.com
+*/
+
+var data = {
+  body: "{}",
+  headers: {
+    "Content-Type": "application/json"
+  }
+};
+$done({response: data});
