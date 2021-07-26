d2d31c11b4248dd03c9623863466840b22dd5acd
佩佩 <admin@nange.cn>
2021-07-27 03:07:50 +0800

    Update.

--- Script/TextNow/TextNow.js
+++ Script/TextNow/TextNow.js
@@ -1,8 +1,10 @@
 /*
 ^https:\/\/api\.textnow\.me\/.+\/users\/.* url script-response-body langkhach/Textnow.js
-*/var obj = JSON.parse($response.body); 
+*/
+var obj = JSON.parse($response.body); 
 obj['state'] = "PREMIUM_SUBSCRIPTION";
 obj['show_ads'] = false;
 obj['premium_calling'] = true;
 obj['platform'] = "TN_IOS";
+obj['error_code'] = "NOT_FOUND";
 $done({body: JSON.stringify(obj)});
