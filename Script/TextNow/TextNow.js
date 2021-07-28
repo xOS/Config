fd366891bd2f5232de8bf52e728bd477ba1344fb
佩佩 <admin@nange.cn>
2021-07-28 22:14:13 +0800

    Update.

--- Script/TextNow/TextNow.js
+++ Script/TextNow/TextNow.js
@@ -1,6 +1,3 @@
-/*
-^https:\/\/api\.textnow\.me\/.+\/users\/.* url script-response-body langkhach/Textnow.js
-*/
 var obj = JSON.parse($response.body);
 obj['features']['cdma_fallback'] = true;
 obj['features']['e911_accepted'] = true;
@@ -20,17 +17,4 @@ obj['result']['show_ads'] = false;
 obj['result']['premium_calling'] = true;
 obj['result']['platform'] = "TN_IOS_PREMIUM";
 obj['result']['code'] = "10000";
-obj['textnow_wireless_store_banner']['banner_enabled'] = 0;
-obj['android']['native_ads']['in_call']['enabled']  = false;
-obj['android']['native_ads']['conversation_list']['enabled']  = false;
-obj['android']['native_ads']['default_native_ad'] = {};
-obj['android']['native_ads']['outgoing_call_message']['enabled']  = false;
-obj['android']['native_ads']['enable_native_ads'] = false;
-obj['ad']['native_ads']['conversation_list']['enabled']  = false;
-obj['ad']['native_ads']['in_call']['enabled']  = false;
-obj['ad']['native_ads']['outgoing_call_message']['enabled']  = false;
-obj['ad']['native_ads']['default_native_ad'] = {};
-obj['ad']['native_ads']['native_announcement'] = {};
-obj['ad']['native_ads']['jsdict'] = [];
-obj['ad']['enable_native_ads']= false;
 $done({body: JSON.stringify(obj)});
