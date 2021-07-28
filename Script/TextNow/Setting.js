8c659c22ade57951b41a2fe9d0b107012ed2cdf1
佩佩 <admin@nange.cn>
2021-07-28 23:24:24 +0800

    Update.

--- Script/TextNow/Setting.js
+++ Script/TextNow/Setting.js
@@ -2,6 +2,7 @@ var obj = JSON.parse($response.body);
 obj['textnow_wireless_store_banner']['banner_enabled'] = 0;
 obj['android']['native_ads'] = {};
 obj['android']['promo_campaign_ad'] = {};
+obj['android']['banner_message'] = {};
 obj['android']['banners'] = [];
 obj['android']['banner_group'] = [];
 obj['ad'] = {};
