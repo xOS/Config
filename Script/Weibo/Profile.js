77e8cefe7b37e1c3ef4345887bd94e1eaca2a419
佩佩 <admin@nange.cn>
2021-08-23 20:49:19 +0800

    Update.

--- Script/Weibo/Profile.js
+++ Script/Weibo/Profile.js
@@ -2,5 +2,7 @@ var obj = JSON.parse($response.body);
 obj['moreInfo']['noMore'] = true;
 items = obj['items'];
 delete items[3];
+recommend = items[5];
+recommend.splice(3);
 items.splice(8);
 $done({body: JSON.stringify(obj)});
