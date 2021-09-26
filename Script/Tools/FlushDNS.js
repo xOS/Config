4f8d87f07b5295edef11732f98f0f8efd1b42a61
佩佩 <admin@nange.cn>
2021-09-26 18:06:13 +0800

    Update DNS Flush.

---
+++ Script/Tools/FlushDNS.js
@@ -0,0 +1,31 @@
+!(async () => {
+    const { wifi, v4 } = $network;
+    const v4IP = v4.primaryAddress;
+    if (!v4IP) {
+        $.done({
+            title: "未连接网络",
+            content: "请检查网络连接",
+            icon: "airplane",
+            "icon-color": "#ff9800"
+        });
+    }
+
+    let dnsCache = (await httpAPI("/v1/dns", "GET")).dnsCache;
+    dnsCache = [...new Set(dnsCache.map((d) => d.server))].toString().replace(/,/g, "\n");
+    await httpAPI("/v1/dns/flush");
+    let delay = ((await httpAPI("/v1/test/dns_delay")).delay * 1000).toFixed(0);
+    $done({
+        title: "DNS 缓存已刷新",
+        content: `延迟：${delay}ms${dnsCache ? `\nDNS 服务器：\n${dnsCache}` : ""}`,
+        icon: "checkmark.circle",
+        "icon-color": "#00AF00"
+    });
+})();
+
+function httpAPI(path = "", method = "POST", body = null) {
+    return new Promise((resolve) => {
+        $httpAPI(method, path, body, (result) => {
+            resolve(result);
+        });
+    });
+}
