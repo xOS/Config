ab1cee3020a23ead393037beaa4545a76b004333
翠花 <admin@nange.cn>
2021-10-25 17:19:47 +0800

    Update Weather_AQI_US

--- Script/Apple/Weather_AQI_US_iOS14.js
+++ Script/Apple/Weather_AQI_US_iOS14.js
@@ -14,8 +14,8 @@ hostname = %APPEND% weather-data.apple.com
 */
 
 const AirQualityStandard = {
-	CN: 'HJ6332012.2111',
-	US: 'EPA_NowCast.2111'
+	CN: 'HJ6332012.2113',
+	US: 'EPA_NowCast.2113'
 }
 
 const AirQualityLevel = {
