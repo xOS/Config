ed3661b516276d50bba89e2c1ca2647a2c9aeea4
翠花 <admin@nange.cn>
2021-12-24 18:58:02 +0800

    Update Mine.js

--- Script/Bilibili/Space.js
+++ Script/Bilibili/Space.js
@@ -3,7 +3,7 @@ obj['data']['card']['official_verify']['type'] = 1;
 obj['data']['card']['official_verify']['desc'] = "超级管理员";
 obj['data']['card']['official_verify']['role'] = 1;
 obj['data']['card']['official_verify']['title'] = "管理员";
-obj['data']['card']['vip']['vipStatus'] = 2;
+obj['data']['card']['vip']['vipStatus'] = 1;
 obj['data']['ad_source_content_v2']['is_ad_loc'] = false;
 obj['data']['ad_source_content_v2']['client_ip'] = "222.178.244.1";
 $done({body: JSON.stringify(obj)});
