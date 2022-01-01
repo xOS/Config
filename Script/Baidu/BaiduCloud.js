dc0203ab317e7edc228eb6b63c64ea1e235231f6
翠花 <admin@nange.cn>
2022-01-01 18:07:01 +0800

    Update.

--- Script/Baidu/BaiduCloud.js
+++ Script/Baidu/BaiduCloud.js
@@ -7,35 +7,60 @@ https:\/\/pan\.baidu\.com\/rest\/2\.0\/membership\/user url script-response-body
 MITM = pan.baidu.com
 */
 
-let obj = JSON.parse($response.body);
 obj = {
-  "product_infos": [{
-    "product_id": "5310897792128633390",
-    "start_time": 1417260485,
-    "end_time": 2147483648,
-    "buy_time": "1417260485",
-    "cluster": "offlinedl",
-    "detail_cluster": "offlinedl",
-    "product_name": "gz_telecom_exp"
-  }, {
-    "product_name": "svip2_nd",
-    "product_description": "超级会员",
-    "function_num": 0,
-    "start_time": 1553702399,
-    "buy_description": "",
-    "buy_time": 0,
-    "product_id": "1",
-    "auto_upgrade_to_svip": 0,
-    "end_time": 1672502399,
-    "cluster": "vip",
-    "detail_cluster": "svip",
-    "status": 0
-  }],
-  "currenttime": 1573473597,
-  "reminder": {
-    "reminderWithContent": [],
-    "advertiseContent": []
+  'product_infos': [
+      {
+          'product_id': '5310897792128633390',
+          'start_time': 1417260485,
+          'end_time': 2147483648,
+          'buy_time': '1417260485',
+          'cluster': 'offlinedl',
+          'detail_cluster': 'offlinedl',
+          'product_name': 'gz_telecom_exp'
+      },
+      {
+          'product_name': 'svip2_nd',
+          'product_description': '超级会员',
+          'function_num': 0,
+          'start_time': 1553702399,
+          'buy_description': '',
+          'buy_time': 0,
+          'product_id': '1',
+          'auto_upgrade_to_svip': 0,
+          'end_time': 2147483648,
+          'cluster': 'vip',
+          'detail_cluster': 'svip',
+          'status': 0
+      }
+  ],
+  "user_type": "svip",
+  "level_info": {
+      "last_manual_collection_time": 0,
+      "current_value": 43000,
+      "history_value": 43000,
+      "accumulated_uncollected_points": 0,
+      "daily_value": 0,
+      "accumulated_lost_points": 0,
+      "current_max_points": 43000,
+      "current_level": 8,
+      "history_level": 7,
+      "default_daily_value": 15,
+      "accumulated_day": 0
+    },
+  'currenttime': 1573473597,
+  'reminder': {
+      'reminderWithContent': [],
+      'advertiseContent': []
   },
-  "request_id": 7501873289383874371
+  'request_id': 7501873289383875000,
+  "status_data": "已是超级会员 SVIP V8",
+  'guide_data': {
+      'title': '超级会员 SVIP',
+      'content': '已拥有极速下载+视频倍速特权',
+      'button': {
+          'text': '会员中心',
+          'action_url': 'https://pan.baidu.com/wap/vip/user?from=myvip2#svip'
+      }
+  }
 };
-$done({body: JSON.stringify(obj)});
+$done({ body: JSON.stringify(obj) });
