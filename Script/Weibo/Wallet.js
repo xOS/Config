d7f69830008dc54ae995c9d14d0746e712c9fd72
翠花 <admin@nange.cn>
2022-01-03 02:32:51 +0800

    Update.

--- Script/Weibo/Wallet.js
+++ Script/Weibo/Wallet.js
@@ -1,4 +1,4 @@
 var obj = JSON.parse($response.body); 
-obj.data.recommend_cards.list = [];
+if (obj.hasOwnProperty('recommend_cards')) obj.data.recommend_cards.list = [];
 obj.data.other_cards = [];
 $done({body: JSON.stringify(obj)});
