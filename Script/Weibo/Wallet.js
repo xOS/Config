89a0762f6de7b6a69ed0686530b45dbd886d50b5
翠花 <admin@nange.cn>
2022-01-03 04:26:59 +0800

    Update.

--- Script/Weibo/Wallet.js
+++ Script/Weibo/Wallet.js
@@ -1,4 +1,25 @@
-var obj = JSON.parse($response.body); 
-if (obj.hasOwnProperty('recommend_cards')) obj.data.recommend_cards.list = [];
-obj.data.other_cards = [];
-$done({body: JSON.stringify(obj)});
+const index = "/index";
+const person = "/person";
+const url = $request.url;
+const obj = JSON.parse($response.body); 
+if (url.indexOf(person) != -1){
+    obj.data.recommend_cards = {};
+    obj.data.other_cards = [];
+    obj.data.popad = {};
+    obj.data.sign = {};
+    obj.data.finance.recommendcard = [];
+    obj.data.finance.extraicon= [];
+    obj.data.ad = [];
+    obj.data.sign_new = {};
+    $done({body: JSON.stringify(obj)});
+} else if(url.indexOf(index) != -1){
+    obj.data.recommend_cards = {};
+    obj.data.other_cards = [];
+    obj.data.popad = {};
+    obj.data.sign = {};
+    // obj.data.finance.recommendcard = [];
+    // obj.data.finance.extraicon= [];
+    obj.data.ad = [];
+    obj.data.sign_new = {};
+    $done({body: JSON.stringify(obj)});
+}
