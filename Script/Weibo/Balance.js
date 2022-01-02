0a7eb9d752a8b2c07ecb383b68ac3665d0d64b15
翠花 <admin@nange.cn>
2022-01-03 02:28:10 +0800

    Update.

--- Script/Weibo/Balance.js
+++ Script/Weibo/Balance.js
@@ -1,8 +1,10 @@
 var obj = JSON.parse($response.body); 
 obj.data.popad = {};
 obj.data.sign = {};
-obj.data.finance.recommendcard = [];
-obj.data.finance.extraicon= [];
+if (obj.hasOwnProperty('finance')) {
+    obj.data.finance.recommendcard = [];
+    obj.data.finance.extraicon= [];
+}
 obj.data.ad = [];
 obj.data.sign_new = {};
 $done({body: JSON.stringify(obj)});
