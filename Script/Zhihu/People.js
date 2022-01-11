471c319b5d622754cd8ed379cd01fa3a5feaa5d0
翠花 <admin@nange.cn>
2022-01-12 07:12:30 +0800

    Update People.js

--- Script/Zhihu/People.js
+++ Script/Zhihu/People.js
@@ -1,9 +1,7 @@
 let body = $response.body 
 body=JSON.parse(body)
 delete body['mcn_user_info']
-body.vip_info = {
-is_vip = true
-};
+body.vip_info.is_vip = true;
 body.is_unicom_free = true;
 body.has_daily_recommend_permission = true;
 body.is_professional = true;
