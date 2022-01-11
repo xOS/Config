cf9ec5fbd28e405003d3fa08a776018ade1e788b
翠花 <admin@nange.cn>
2022-01-12 07:05:54 +0800

    Update Advertising.sgmodule

--- Script/Zhihu/People.js
+++ Script/Zhihu/People.js
@@ -1,6 +1,12 @@
 let body = $response.body 
 body=JSON.parse(body)
 delete body['mcn_user_info']
+body.vip_info = {
+is_vip = true
+};
+body.is_unicom_free = true;
+body.has_daily_recommend_permission = true;
+body.is_professional = true;
 body=JSON.stringify(body)
 $done({body})
 
