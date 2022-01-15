81f361d57236dda3b0eaaf820bd24fc9847e08c9
翠花 <admin@nange.cn>
2022-01-15 20:28:09 +0800

    Update VIPCenter.js

--- Script/Weibo/VIPCenter.js
+++ Script/Weibo/VIPCenter.js
@@ -7,6 +7,13 @@ obj.data.baseInfo.user_info.curtab.cashier_tab_id = '0';
 obj.data.baseInfo.user_info.mbtype = 12;
 obj.data.baseInfo.user_info.identity = '1,0';
 obj.data.baseInfo.user_info.svip_desc = '您已经是微博高级会员';
+obj.data.baseInfo.user_info.vip_identity = 1;
+obj.data.baseInfo.vip_identity = 1;
+if(obj.data.gobuy){
+    obj.data.gobuy = {};
+}
+obj.data.card_list[0].show_act = false;
+obj.data.card_list[0].actbg_config = {};
 obj.data.card_list[0].img = 'https://h5.sinaimg.cn/upload/1004/14/2021/01/22/VIIPcard3x.png';
 obj.data.card_list[0].btn_config.link = 'sinaweibo://mppopupwindow?wbx_hide_close_btn=true&wbx_bg_view_dismiss=true&scheme=sinaweibo%3A%2F%2Fwbox%3Fid%3Dn1htatg0fm%26page%3Dpages%2Fcashier%2Fcashier%26cashier_id%3D3%26F%3Dvipcenter_userinfo_t_hy';
 obj.data.card_list[0].bg_config.img = 'https://h5.sinaimg.cn/upload/1004/14/2021/01/22/VIPbackground3x.png';
@@ -17,6 +24,9 @@ if (obj.data.baseInfo.user_info.type != 1){
     obj.data.card_list.splice(1,i);
 }
 else if (obj.data.card_list) {
+    obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '1520'));
+    obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '3238'));
+    obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '1460'));
     obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '2630'));
     obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '2080'));
     obj.data.card_list = obj.data.card_list.filter(element => !(element['m_id'] == '2480'));
