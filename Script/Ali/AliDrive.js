7759d32497d749547fb0be81688f66dfbda789ff
翠花 <admin@nange.cn>
2022-01-30 02:11:06 +0800

    Add AliDrive

---
+++ Script/Ali/AliDrive.js
@@ -0,0 +1,5 @@
+let body = JSON.parse($response.body);
+if (body.result) {
+    body.result = body.result.filter(element => !(element['appCode'] == 'subscription'));
+}
+$done({ body: JSON.stringify(body) });
