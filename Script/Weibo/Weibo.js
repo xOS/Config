032e00cc264d1eec21a22e492f84e65bf2e1f5ab
翠花 <admin@nange.cn>
2022-02-24 17:29:47 +0800

    Update Weibo.js

--- Script/Weibo/Weibo.js
+++ Script/Weibo/Weibo.js
@@ -374,10 +374,10 @@ function removeMediaHomelist(data) {
 
 //评论区相关和推荐内容
 function removeComments(data) {
-    let delType = [];
+    let delType = ['广告'];
     if (mainConfig.removeRelateItem) delType.push('相关内容');
     if (mainConfig.removeRecommendItem) delType.push('推荐');
-    if (delType.length === 0) return;
+    // if (delType.length === 0) return;
     let items = data.datas || [];
     if (items.length === 0) return;
     let newItems = [];
