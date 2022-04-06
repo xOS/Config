7cb4713b9ddb8662394767ef4ede8a254974106a
翠花 <admin@nange.cn>
2022-04-06 18:25:08 +0800

    Update.

--- Script/Hostloc/ClearURL.js
+++ Script/Hostloc/ClearURL.js
@@ -5,21 +5,31 @@ if (/<\/html>|<\/body>/.test(body)) {
     body = body.replace('</body>', `
   
   <script>
-// Hostloc 移除 AFF，URL 超链接化
-var content = document.querySelectorAll(".pcb");
-const regex = new RegExp('(?<!font size="2"><a href=")((?<!src="|href=")https?:\/\/.*?)(<|&nbsp;)', 'gm');
 
-for (var i = 0; i < content.length; i++) {
-    //console.log(content[i].innerHTML);
-    //console.log(regex.test(content[i].innerHTML));
+// @description  Hostloc 移除 AFF，URL 超链接化
+// @author       Faxlok
+// @match        https://hostloc.com/thread-*.html
+// @match        https://hostloc.com/forum.php?mod=viewthread&tid=*&highlight=*
+// @match        https://hostloc.com/forum.php?mod=viewthread&tid=*&page=*
 
-    // 转换为超链接
-    if (regex.test(content[i].innerHTML)) {
-        content[i].innerHTML = content[i].innerHTML.replace(regex, '<a href="$1" target="_blank">$1</a><');
+(function () {
+    'use strict';
+    // https://regex101.com/r/sZaYtI/1
+    var content = document.querySelectorAll(".pcb");
+    const regex = new RegExp('(?<!font size="2"><a href=")((?<!src="|href=")https?:\/\/.*?)(<|&nbsp;)', 'gm');
+
+    for (var i = 0; i < content.length; i++) {
+        //console.log(content[i].innerHTML);
+        //console.log(regex.test(content[i].innerHTML));
+
+        // URL 转为超链接
+        if (regex.test(content[i].innerHTML)) {
+            content[i].innerHTML = content[i].innerHTML.replace(regex, '<a href="$1" target="_blank">$1</a><');
+        }
+        // 去掉 AFF
+        content[i].innerHTML = content[i].innerHTML.replace(/aff\.php/gm, 'cart.php').replace(/aff=\d+/gm, 'a=add').replace(/\/aff\/\w*|ref=\w+/gm, '');
     }
-    // 去掉aff
-    content[i].innerHTML = content[i].innerHTML.replace(/aff\.php/gm, 'cart.php').replace(/aff=\d+/gm, 'a=add').replace(/ref=\w+/gm, '');
-}
+})();
 </script></body>`)
 }
 $done({ body });
