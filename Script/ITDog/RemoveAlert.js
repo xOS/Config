182b9ad008622d1ff5c63893ef160b31ce453497
翠花 <admin@nange.cn>
2022-04-09 15:48:43 +0800

    Update.

--- Script/ITDog/RemoveAlert.js
+++ Script/ITDog/RemoveAlert.js
@@ -5,19 +5,19 @@ if (/<\/html>|<\/body>/.test(body)) {
     body = body.replace('</body>', `
 
 <script>
-(function() {
-    'use strict';
-    var content = document.querySelectorAll("script");
+// (function() {
+//     'use strict';
+//     var content = document.querySelectorAll("script");
     
-    for (var i = 0; i < content.length; i++)
-    {
-        if(content[i].innerHTML.toString().indexOf('_0xodd=') != -1){
-            content[i].innerHTML = null;
-            break;
-        }
-        // break;
-    }
-})();
+//     for (var i = 0; i < content.length; i++)
+//     {
+//         if(content[i].innerHTML.toString().indexOf('_0xodd=') != -1){
+//             content[i].innerHTML = null;
+//             break;
+//         }
+//         // break;
+//     }
+// })();
 window.alert = function() {
     return false;
 }
