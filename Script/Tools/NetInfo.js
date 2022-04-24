a8c464f7a6993bb9b89a6c4b4c6509ff46354b36
翠花 <admin@nange.cn>
2022-04-24 15:41:50 +0800

    Update.

--- Script/Tools/NetInfo.js
+++ Script/Tools/NetInfo.js
@@ -69,7 +69,7 @@
         //  const state = json['state'];
  
          const externalIP = data.toString().split("\n")[0];
-         const info = data.toString().split("\n")[1];
+         const info = data.toString().split("\n")[1].replace(/中国\n$/gm, "");
  
         //  if (state == 'CN') {
         //      if (city && !district) {
