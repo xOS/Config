87f5b0eed4fb37d93a48e1b3608e887ba5f987a4
翠花 <admin@nange.cn>
2022-04-27 05:08:50 +0800

    Update BlackUser.js

--- Script/Poco/BlackUser.js
+++ Script/Poco/BlackUser.js
@@ -2,18 +2,19 @@ let body = $response.body;
 body = JSON.parse(body);
 
 // 黑名单
-let blackid = [178336580, 201494286, 201843361, 178336580, 201919782, 200605457];
+let blackId = [178336580, 201494286, 201843361, 178336580, 201919782, 200605457];
+
 if (body.data && body.data.list && body.data.list.length > 0) {
     var data = body.data.list;
     for (var i in data) {
         let uid = data[i].user_id;
         let nickname = data[i].user_nickname;
-        for (var k in blackid) {
-            if (uid === blackid[k]) {
+        for (var k in blackId) {
+            if (uid === blackId[k]) {
                 data[i] = null;
                 data.splice(i, 1);
-                console.log("屏蔽用户名："+ nickname);
-                console.log("屏蔽用户ID："+ uid);
+                console.log(nickname + "已被屏蔽！");
+                // console.log("屏蔽用户ID："+ uid);
             }
         }
         
