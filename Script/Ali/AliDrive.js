6b5dd4d6980e4d4b9b14f7b72f4a016a8e0d5ca4
翠花 <admin@nange.cn>
2022-05-02 05:08:01 +0800

    Update GeneralRule.sgmodule

--- Script/Ali/AliDrive.js
+++ Script/Ali/AliDrive.js
@@ -1,5 +1,7 @@
 let body = JSON.parse($response.body);
 if (body.result) {
-    body.result = body.result.filter(element => !(element['appCode'] == (subscription|giftshop|adrive)));
+    body.result = body.result.filter(element => !(element['appCode'] == 'subscription'));
+    body.result = body.result.filter(element => !(element['appCode'] == 'giftshop'));
+    body.result = body.result.filter(element => !(element['appCode'] == 'adrive')));
 }
 $done({ body: JSON.stringify(body) });
