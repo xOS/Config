4adb4dbd286a9cff64c62dbf7c2eff10ee66787e
翠花 <admin@nange.cn>
2022-05-02 04:58:58 +0800

    Update BlockAdvertising.sgmodule

--- Script/Ali/AliDrive.js
+++ Script/Ali/AliDrive.js
@@ -1,5 +1,5 @@
 let body = JSON.parse($response.body);
 if (body.result) {
-    body.result = body.result.filter(element => !(element['appCode'] == 'subscription'));
+    body.result = body.result.filter(element => !(element['appCode'] == (subscription|giftshop|adrive)));
 }
 $done({ body: JSON.stringify(body) });
