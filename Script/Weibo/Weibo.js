4a4a8e97f08215e9f4498c98e8d7f76239ba744c
翠花 <admin@nange.cn>
2022-05-10 12:10:33 +0800

    Update Weibo.js

--- Script/Weibo/Weibo.js
+++ Script/Weibo/Weibo.js
@@ -402,7 +402,7 @@ function removeHome(data) {
                     newItems.push(item);
                 }
             }
-        } else if (['mine_attent_title', '100505_-_meattent_pic', '100505_-_newusertask', '100505_-_vipkaitong', '100505_-_hongbao2022'].indexOf(itemId) > -1) {
+        } else if (['mine_attent_title', '100505_-_meattent_pic', '100505_-_newusertask', '100505_-_vipkaitong', '100505_-_hongbao2022', '100505_-_adphoto'].indexOf(itemId) > -1) {
             continue;
         } else if (itemId.match(/100505_-_meattent_-_\d+/)) {
             continue;
