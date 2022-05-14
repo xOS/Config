fafdea59f274a6816b4898a49940ed555394df80
翠花 <admin@nange.cn>
2022-05-14 17:27:25 +0800

    Update.

--- Script/Apple/Weather.js
+++ Script/Apple/Weather.js
@@ -2,8 +2,8 @@
 README:https://github.com/VirgilClyne/iRingo
 */
 
-const $ = new Env("Apple Weather v3.2.7");
-const URL = new URLSearch();
+const $ = new Env("Apple Weather v3.2.8");
+const URL = new URLs();
 const DataBase = {
 	"Weather":{"Switch":true,"NextHour":{"Switch":true},"AQI":{"Switch":true,"Mode":"WAQI Private","Location":"Station","Auth":'f32078323b1346754995d08b1e3ef0f24c411f9b',"Scale":"EPA_NowCast.2204"},"Map":{"AQI":true}},
 	"Siri":{"Switch":true,"CountryCode":"TW","Domains":["web","itunes","app_store","movies","restaurants","maps"],"Functions":["flightutilities","lookup","mail","messages","news","safari","siri","spotlight","visualintelligence"],"Safari_Smart_History":true},
@@ -622,7 +622,7 @@ async function outputNextHour(apiVersion, providerName, minutelyData, weather, S
 	};
 
 	function getMinutes(apiVersion, minutely, startTime) {
-		$.log(`🚧 ${$.name}, 开始设置Minutes`, '');
+		//$.log(`🚧 ${$.name}, 开始设置Minutes`, '');
 		let minutes = minutely.precipitation_2h.map((value, index) => {
 			let minute = {
 				"precipIntensity": value,
@@ -637,12 +637,12 @@ async function outputNextHour(apiVersion, providerName, minutelyData, weather, S
 			}
 			return minute
 		});
-		$.log(`🚧 ${$.name}, minutes = ${JSON.stringify(minutes)}`, '');
+		//$.log(`🚧 ${$.name}, minutes = ${JSON.stringify(minutes)}`, '');
 		return minutes;
 	};
 
 	function getConditions(apiVersion, minutelyData, minutes) {
-		// $.log(`🚧 ${$.name}, 开始设置conditions`, "");
+		$.log(`🚧 ${$.name}, 开始设置conditions`, "");
 		// TODO: when to add possible
 		const ADD_POSSIBLE_UPPER = 0;
 		const POSSIBILITY = { POSSIBLE: "possible" };
@@ -956,97 +956,77 @@ async function outputNextHour(apiVersion, providerName, minutelyData, weather, S
 	};
 
 	function getSummaries(apiVersion, minutes) {
-		// $.log(`🚧 ${$.name}, 开始设置summary`, "");
+		$.log(`🚧 ${$.name}, 开始设置summary`, "");
 		const weatherType = getWeatherType(minutelyData?.result?.hourly);
 		$.log(`🚧 ${$.name}, weatherType = ${weatherType}`, '');
 
-		const summaries = [];
-
 		// initialize data
+		let summaries = [];
 		let lastIndex = 0;
-		// little trick for origin data
-		let isRainOrSnow = minutes[0].precipIntensity > 0;
-		let summary = {
-			// I guess data from weatherType is not always reliable
-			condition: isRainOrSnow ? weatherType : SUMMARY_CONDITION_TYPES.CLEAR,
-		};
-		if (apiVersion !== "v1") summary.startTime = minutes[0].startTime;
+		//minutes = minutes.slice(0, 80);
 
-		minutes.slice(0, 60).forEach((minute, index, array) => {
-			// clear in an hour
-			// Apple weather could only display one hour data
-			// drop useless data to avoid display empty graph
-			if (index + 1 >= array.length && !isRainOrSnow) {
+		for (let index = 0; index < minutes.length; index++) {
+			const { startAt, startTime, precipIntensity } = minutes[index];
+			const PL = calculatePL(precipIntensity);
+			const prevPL = (index !== 0) ? calculatePL(minutes?.[index - 1]?.precipIntensity) : PL;
+
+			if (prevPL && !PL) { // 有到无
+				$.log(`🚧 ${$.name}, 有到无`, "");
+				// for find the max value of precipChance and precipIntensity
+				const range = minutes.slice(lastIndex, index);
+				// initialize data
+				let summary = { condition: weatherType };
+				if (apiVersion == "v1") {
+					summary.validUntil = startAt
+					summary.probability = Math.max(...range.map(value => value.precipChance));
+					// it looks like Apple doesn't care precipIntensity
+					summary.maxIntensity = Math.max(...range.map(value => value.precipIntensity));
+					summary.minIntensity = Math.min(...range.map(value => value.precipIntensity));
+				} else {
+					summary.startTime = minutes[lastIndex].startTime;
+					summary.endTime = startTime;
+					summary.precipChance = Math.max(...range.map(value => value.precipChance));
+					// it looks like Apple doesn't care precipIntensity
+					summary.precipIntensity = Math.max(...range.map(value => value.precipIntensity));
+				};
+				lastIndex = index;
 				summaries.push(summary);
-				//$.log(`🚧 ${$.name}, summaries = ${JSON.stringify(summaries)}`, "");
-			}
-
-			// this loop will handle previous condition and create the condition for next condition
-			// `startAt` for APIv1, `startTime` for APIv2
-			// is this too dirty?
-			const { startAt, startTime, precipIntensity } = minute;
-			if (isRainOrSnow) {
-				if (
-					// end of rain
-					calculatePL(precipIntensity) === PRECIPITATION_LEVEL.NO_RAIN_OR_SNOW ||
-					// constant of rain
-					// we always need precipChance and precipIntensity data
-					index + 1 === array.length
-				) {
+			} else if (!prevPL && PL) { // 无到有
+				$.log(`🚧 ${$.name}, 无到有`, "");
+				// initialize data
+				let summary = { condition: "clear" };
+				if (apiVersion == "v1") summary.validUntil = startAt;
+				else {
+					summary.startTime = minutes[lastIndex].startTime;
+					summary.endTime = startTime;
+				}
+				lastIndex = index;
+				summaries.push(summary);
+			} else if (index + 1 == minutes.length) { // 到结尾
+				$.log(`🚧 ${$.name}, 到结尾`, "");
+				// initialize data
+				let summary = { condition: "clear" };
+				if (apiVersion !== "v1") summary.startTime = minutes[lastIndex].startTime;
+				if (PL) { // 到结尾还在下
 					// for find the max value of precipChance and precipIntensity
-					const range = minutes.slice(lastIndex, index + 1);
-
-					// we reach the data end but cannot find the end of rain
-					if (calculatePL(precipIntensity) === PRECIPITATION_LEVEL.NO_RAIN_OR_SNOW) {
-						if (apiVersion == "v1") summary.validUntil = startAt
-						else summary.endTime = startTime;
-					}
-
+					const range = minutes.slice(lastIndex, index);
+					summary.condition = weatherType;
 					if (apiVersion == "v1") {
 						summary.probability = Math.max(...range.map(value => value.precipChance));
 						// it looks like Apple doesn't care precipIntensity
 						summary.maxIntensity = Math.max(...range.map(value => value.precipIntensity));
 						summary.minIntensity = Math.min(...range.map(value => value.precipIntensity));
 					} else {
+						summary.startTime = minutes[lastIndex].startTime;
 						summary.precipChance = Math.max(...range.map(value => value.precipChance));
 						// it looks like Apple doesn't care precipIntensity
 						summary.precipIntensity = Math.max(...range.map(value => value.precipIntensity));
 					};
-
-					summaries.push(summary);
-
-					// reset summary
-					isRainOrSnow = !isRainOrSnow;
-					lastIndex = index;
-
-					if (apiVersion == "v1") {
-						summary = { condition: SUMMARY_CONDITION_TYPES.CLEAR };
-					} else {
-						summary = {
-							startTime: startTime,
-							condition: SUMMARY_CONDITION_TYPES.CLEAR,
-						}
-					};
-				} else {
-					if (calculatePL(precipIntensity) > PRECIPITATION_LEVEL.NO_RAIN_OR_SNOW) {
-						if (apiVersion == "v1") summary.validUntil = startAt;
-						else summary.endTime = startTime;
-
-						summaries.push(summary);
-
-						// reset summary
-						isRainOrSnow = !isRainOrSnow;
-						lastIndex = index;
-
-						if (apiVersion == "v1") summary = { condition: weatherType };
-						else summary = {
-							startTime: startTime,
-							condition: weatherType,
-						};
-					}
 				}
-			}
-		});
+				// 不管还下不下，结尾都要push一次
+				summaries.push(summary);
+			};
+		};
 		$.log(`🚧 ${$.name}, summaries = ${JSON.stringify(summaries)}`, "");
 		return summaries;
 	};
@@ -1149,5 +1129,5 @@ function Metadata(input = { "Version": new Number, "Time": new Date, "Expire": n
 // https://github.com/chavyleung/scripts/blob/master/Env.min.js
 function Env(t,e){class s{constructor(t){this.env=t}send(t,e="GET"){t="string"==typeof t?{url:t}:t;let s=this.get;return"POST"===e&&(s=this.post),new Promise((e,i)=>{s.call(this,t,(t,s,r)=>{t?i(t):e(s)})})}get(t){return this.send.call(this.env,t)}post(t){return this.send.call(this.env,t,"POST")}}return new class{constructor(t,e){this.name=t,this.http=new s(this),this.data=null,this.dataFile="box.dat",this.logs=[],this.isMute=!1,this.isNeedRewrite=!1,this.logSeparator="\n",this.encoding="utf-8",this.startTime=(new Date).getTime(),Object.assign(this,e),this.log("",`\ud83d\udd14${this.name}, \u5f00\u59cb!`)}isNode(){return"undefined"!=typeof module&&!!module.exports}isQuanX(){return"undefined"!=typeof $task}isSurge(){return"undefined"!=typeof $httpClient&&"undefined"==typeof $loon}isLoon(){return"undefined"!=typeof $loon}isShadowrocket(){return"undefined"!=typeof $rocket}isStash(){return"undefined"!=typeof $environment&&$environment["stash-version"]}toObj(t,e=null){try{return JSON.parse(t)}catch{return e}}toStr(t,e=null){try{return JSON.stringify(t)}catch{return e}}getjson(t,e){let s=e;const i=this.getdata(t);if(i)try{s=JSON.parse(this.getdata(t))}catch{}return s}setjson(t,e){try{return this.setdata(JSON.stringify(t),e)}catch{return!1}}getScript(t){return new Promise(e=>{this.get({url:t},(t,s,i)=>e(i))})}runScript(t,e){return new Promise(s=>{let i=this.getdata("@chavy_boxjs_userCfgs.httpapi");i=i?i.replace(/\n/g,"").trim():i;let r=this.getdata("@chavy_boxjs_userCfgs.httpapi_timeout");r=r?1*r:20,r=e&&e.timeout?e.timeout:r;const[o,h]=i.split("@"),n={url:`http://${h}/v1/scripting/evaluate`,body:{script_text:t,mock_type:"cron",timeout:r},headers:{"X-Key":o,Accept:"*/*"}};this.post(n,(t,e,i)=>s(i))}).catch(t=>this.logErr(t))}loaddata(){if(!this.isNode())return{};{this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e);if(!s&&!i)return{};{const i=s?t:e;try{return JSON.parse(this.fs.readFileSync(i))}catch(t){return{}}}}}writedata(){if(this.isNode()){this.fs=this.fs?this.fs:require("fs"),this.path=this.path?this.path:require("path");const t=this.path.resolve(this.dataFile),e=this.path.resolve(process.cwd(),this.dataFile),s=this.fs.existsSync(t),i=!s&&this.fs.existsSync(e),r=JSON.stringify(this.data);s?this.fs.writeFileSync(t,r):i?this.fs.writeFileSync(e,r):this.fs.writeFileSync(t,r)}}lodash_get(t,e,s){const i=e.replace(/\[(\d+)\]/g,".$1").split(".");let r=t;for(const t of i)if(r=Object(r)[t],void 0===r)return s;return r}lodash_set(t,e,s){return Object(t)!==t?t:(Array.isArray(e)||(e=e.toString().match(/[^.[\]]+/g)||[]),e.slice(0,-1).reduce((t,s,i)=>Object(t[s])===t[s]?t[s]:t[s]=Math.abs(e[i+1])>>0==+e[i+1]?[]:{},t)[e[e.length-1]]=s,t)}getdata(t){let e=this.getval(t);if(/^@/.test(t)){const[,s,i]=/^@(.*?)\.(.*?)$/.exec(t),r=s?this.getval(s):"";if(r)try{const t=JSON.parse(r);e=t?this.lodash_get(t,i,""):e}catch(t){e=""}}return e}setdata(t,e){let s=!1;if(/^@/.test(e)){const[,i,r]=/^@(.*?)\.(.*?)$/.exec(e),o=this.getval(i),h=i?"null"===o?null:o||"{}":"{}";try{const e=JSON.parse(h);this.lodash_set(e,r,t),s=this.setval(JSON.stringify(e),i)}catch(e){const o={};this.lodash_set(o,r,t),s=this.setval(JSON.stringify(o),i)}}else s=this.setval(t,e);return s}getval(t){return this.isSurge()||this.isLoon()?$persistentStore.read(t):this.isQuanX()?$prefs.valueForKey(t):this.isNode()?(this.data=this.loaddata(),this.data[t]):this.data&&this.data[t]||null}setval(t,e){return this.isSurge()||this.isLoon()?$persistentStore.write(t,e):this.isQuanX()?$prefs.setValueForKey(t,e):this.isNode()?(this.data=this.loaddata(),this.data[e]=t,this.writedata(),!0):this.data&&this.data[e]||null}initGotEnv(t){this.got=this.got?this.got:require("got"),this.cktough=this.cktough?this.cktough:require("tough-cookie"),this.ckjar=this.ckjar?this.ckjar:new this.cktough.CookieJar,t&&(t.headers=t.headers?t.headers:{},void 0===t.headers.Cookie&&void 0===t.cookieJar&&(t.cookieJar=this.ckjar))}get(t,e=(()=>{})){if(t.headers&&(delete t.headers["Content-Type"],delete t.headers["Content-Length"]),this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient.get(t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let s=require("iconv-lite");this.initGotEnv(t),this.got(t).on("redirect",(t,e)=>{try{if(t.headers["set-cookie"]){const s=t.headers["set-cookie"].map(this.cktough.Cookie.parse).toString();s&&this.ckjar.setCookieSync(s,null),e.cookieJar=this.ckjar}}catch(t){this.logErr(t)}}).then(t=>{const{statusCode:i,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:i,statusCode:r,headers:o,rawBody:h},s.decode(h,this.encoding))},t=>{const{message:i,response:r}=t;e(i,r,r&&s.decode(r.rawBody,this.encoding))})}}post(t,e=(()=>{})){const s=t.method?t.method.toLocaleLowerCase():"post";if(t.body&&t.headers&&!t.headers["Content-Type"]&&(t.headers["Content-Type"]="application/x-www-form-urlencoded"),t.headers&&delete t.headers["Content-Length"],this.isSurge()||this.isLoon())this.isSurge()&&this.isNeedRewrite&&(t.headers=t.headers||{},Object.assign(t.headers,{"X-Surge-Skip-Scripting":!1})),$httpClient[s](t,(t,s,i)=>{!t&&s&&(s.body=i,s.statusCode=s.status),e(t,s,i)});else if(this.isQuanX())t.method=s,this.isNeedRewrite&&(t.opts=t.opts||{},Object.assign(t.opts,{hints:!1})),$task.fetch(t).then(t=>{const{statusCode:s,statusCode:i,headers:r,body:o}=t;e(null,{status:s,statusCode:i,headers:r,body:o},o)},t=>e(t));else if(this.isNode()){let i=require("iconv-lite");this.initGotEnv(t);const{url:r,...o}=t;this.got[s](r,o).then(t=>{const{statusCode:s,statusCode:r,headers:o,rawBody:h}=t;e(null,{status:s,statusCode:r,headers:o,rawBody:h},i.decode(h,this.encoding))},t=>{const{message:s,response:r}=t;e(s,r,r&&i.decode(r.rawBody,this.encoding))})}}time(t,e=null){const s=e?new Date(e):new Date;let i={"M+":s.getMonth()+1,"d+":s.getDate(),"H+":s.getHours(),"m+":s.getMinutes(),"s+":s.getSeconds(),"q+":Math.floor((s.getMonth()+3)/3),S:s.getMilliseconds()};/(y+)/.test(t)&&(t=t.replace(RegExp.$1,(s.getFullYear()+"").substr(4-RegExp.$1.length)));for(let e in i)new RegExp("("+e+")").test(t)&&(t=t.replace(RegExp.$1,1==RegExp.$1.length?i[e]:("00"+i[e]).substr((""+i[e]).length)));return t}msg(e=t,s="",i="",r){const o=t=>{if(!t)return t;if("string"==typeof t)return this.isLoon()?t:this.isQuanX()?{"open-url":t}:this.isSurge()?{url:t}:void 0;if("object"==typeof t){if(this.isLoon()){let e=t.openUrl||t.url||t["open-url"],s=t.mediaUrl||t["media-url"];return{openUrl:e,mediaUrl:s}}if(this.isQuanX()){let e=t["open-url"]||t.url||t.openUrl,s=t["media-url"]||t.mediaUrl,i=t["update-pasteboard"]||t.updatePasteboard;return{"open-url":e,"media-url":s,"update-pasteboard":i}}if(this.isSurge()){let e=t.url||t.openUrl||t["open-url"];return{url:e}}}};if(this.isMute||(this.isSurge()||this.isLoon()?$notification.post(e,s,i,o(r)):this.isQuanX()&&$notify(e,s,i,o(r))),!this.isMuteLog){let t=["","==============\ud83d\udce3\u7cfb\u7edf\u901a\u77e5\ud83d\udce3=============="];t.push(e),s&&t.push(s),i&&t.push(i),console.log(t.join("\n")),this.logs=this.logs.concat(t)}}log(...t){t.length>0&&(this.logs=[...this.logs,...t]),console.log(t.join(this.logSeparator))}logErr(t,e){const s=!this.isSurge()&&!this.isQuanX()&&!this.isLoon();s?this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t.stack):this.log("",`\u2757\ufe0f${this.name}, \u9519\u8bef!`,t)}wait(t){return new Promise(e=>setTimeout(e,t))}done(t={}){const e=(new Date).getTime(),s=(e-this.startTime)/1e3;this.log("",`\ud83d\udd14${this.name}, \u7ed3\u675f! \ud83d\udd5b ${s} \u79d2`),this.log(),(this.isSurge()||this.isQuanX()||this.isLoon())&&$done(t)}}(t,e)}
 
-// https://github.com/VirgilClyne/iRingo/blob/main/function/URLSearch.min.js
-function URLSearch(s){return new class{constructor(s=[]){this.name="urlParams v1.0.0",this.opts=s,this.json={url:{scheme:"",host:"",path:""},params:{}}}parse(s){let t=s.match(/(?<scheme>.+):\/\/(?<host>[^/]+)\/?(?<path>[^?]+)?\??(?<params>.*)?/)?.groups??null;return t?.params&&(t.params=Object.fromEntries(t.params.split("&").map((s=>s.split("="))))),t}stringify(s=this.json){return s?.params?s.scheme+"://"+s.host+"/"+s.path+"?"+Object.entries(s.params).map((s=>s.join("="))).join("&"):s.scheme+"://"+s.host+"/"+s.path}}(s)}
+// https://github.com/VirgilClyne/VirgilClyne/blob/main/function/URL/URLs.embedded.min.js
+function URLs(s){return new class{constructor(s=[]){this.name="URL v1.0.0",this.opts=s,this.json={url:{scheme:"",host:"",path:""},params:{}}}parse(s){let t=s.match(/(?<scheme>.+):\/\/(?<host>[^/]+)\/?(?<path>[^?]+)?\??(?<params>.*)?/)?.groups??null;return t?.params&&(t.params=Object.fromEntries(t.params.split("&").map((s=>s.split("="))))),t}stringify(s=this.json){return s?.params?s.scheme+"://"+s.host+"/"+s.path+"?"+Object.entries(s.params).map((s=>s.join("="))).join("&"):s.scheme+"://"+s.host+"/"+s.path}}(s)}
