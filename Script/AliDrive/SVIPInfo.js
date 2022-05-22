a3ba201e2703ea99928b72f81b8e64a2355719f6
翠花 <admin@nange.cn>
2022-05-22 20:21:00 +0800

    Update.

--- Script/AliDrive/SVIPInfo.js
+++ Script/AliDrive/SVIPInfo.js
@@ -1,16 +1,24 @@
 let body = JSON.parse($response.body);
-if (body) {
-  body = {
-    "vipList": [
-      {
-        "code": "svip",
-        "promotedAt": 1651254422,
-        "expire": 4102349206,
-        "name": "超级会员"
-      }
-    ],
-    "identity": "svip",
-    "icon": "https:\/\/gw.alicdn.com\/imgextra\/i1\/O1CN01p5OON61LmDrgkaaGT_!!6000000001341-2-tps-40-40.png"
+const personal = "/personal";
+const svip = "/vip/info";
+const url = $request.url;
+
+if (url.indexOf(svip) != -1) {
+  if (body.vipList) {
+    body.vipList[0] = {
+      "code": "svip",
+      "promotedAt": 1651254422,
+      "expire": 4102349206,
+      "name": "超级会员"
+    };
+    body.identity = 'svip';
   }
 }
+if (url.indexOf(personal) != -1) {
+  if (body.personal_rights_info) {
+    body.personal_rights_info.spu_id = 'svip';
+    body.personal_rights_info.name = '超级会员';
+  }
+}
+
 $done({ body: JSON.stringify(body) });
