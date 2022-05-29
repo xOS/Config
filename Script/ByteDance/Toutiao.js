38ffdecc36215425b75fb2900c9246c744964234
翠花 <admin@nange.cn>
2022-05-30 03:04:17 +0800

    Update.

---
+++ Script/ByteDance/Toutiao.js
@@ -0,0 +1,37 @@
+let body = JSON.parse($response.body);
+const feed = "\/api\/news\/feed\/v88";
+const wallet = "\/wallet\/portal\/api\/v3\/prefetch\/settings";
+const url = $request.url;
+
+if (body.data) {
+    if (url.indexOf(feed) != -1) {
+        if (body.data.length > 0) {
+            var data = body.data;
+            for (var i in data) {
+                let content = JSON.parse(data[i].content);
+                if (content.abstract == '' || content.card_title == '小视频' || content.video_source == 'ugc_video' || content.video_style == 2 || content.has_video == true) {
+                    data[i] = {};
+                    //break;
+                }
+            }
+        }
+    }
+
+    if (url.indexOf(wallet) != -1) {
+        if (body.data.GetSettings.Sections) {
+            body.data.GetSettings.Sections[3].services = body.data.GetSettings.Sections[3].services.filter(element => !(element['id'] == '3'));
+
+            body.data.GetSettings.Sections[3].services = body.data.GetSettings.Sections[3].services.filter(element => !(element['id'] == '13'));
+
+            body.data.GetSettings.Sections[3].services = body.data.GetSettings.Sections[3].services.filter(element => !(element['id'] == '4'));
+
+            body.data.GetSettings.Sections[3].services = body.data.GetSettings.Sections[3].services.filter(element => !(element['id'] == '2'));
+            body.data.GetSettings.Sections = body.data.GetSettings.Sections.filter(element => !(element['componentName'] == 'activity-banner'));
+
+            body.data.GetSettings.Sections = body.data.GetSettings.Sections.filter(element => !(element['componentName'] == 'cswiper'));
+            body.data.GetSettings.Sections = body.data.GetSettings.Sections.filter(element => !(element['label'] == '第三方服务'));
+        }
+    }
+}
+
+$done({ body: JSON.stringify(body) });
--- Script/QMNovel/UserInfo.js
+++ Script/QMNovel/UserInfo.js
@@ -1,3 +1,55 @@
 var obj = JSON.parse($response.body);
-obj['data']['is_vip'] = '1';
+const userinfo = "/get-user-info";
+const getinfo = "/get-role-adv-vip-info";
+const vipindex = "/vip/index";
+const center = "/my-center,";
+const userpage = "/page";
+const level = "/my-level";
+const url = $request.url;
+
+if (obj.data) {
+    if (url.indexOf(userinfo) != -1) {
+        obj['data']['is_vip'] = '1';
+    }
+
+    if (url.indexOf(getinfo) != -1 || url.indexOf(vipindex) != -1) {
+        obj['data']['users']['isLifetimeVip'] = 1;
+        obj['data']['users']['year_vip_show'] = 1;
+        obj['data']['users']['isvip'] = 1;
+        obj['data']['year_vip_show'] = 1;
+        obj['data']['rewardVideoConfig'] = {};
+        obj['data']['content'] = [];
+    }
+
+    if (url.indexOf(center) != -1) {
+        obj.data.user_area.vip_info.deadline_date = '永久会员';
+        obj.data.user_area.vip_info.vip_privilege_desc = '您已经是尊贵的永久VIP会员。';
+        // obj.data.user_area.vip_info = {};
+        obj.data.user_area.base_info.year_vip_show = 1;
+        // obj.data.user_area.base_info.level = 50;
+        obj.data.user_area.base_info.is_vip = 1;
+        obj.data.user_area.base_info.vip_show_type = 3;
+        obj.data.user_area.base_info.level_icon = 'https://cdn.wtzw.com/bookimg/free/images/app/1_0_0/level/level_icon_50.png';
+        obj.data.user_area.grid_info[0].num = '999999';
+        obj.data.user_area.grid_info[0].coin_to_money = '99999';
+        // obj.data.func_area[1] = {};
+        obj.data.func_area.splice(1, 1);
+    }
+
+    if (url.indexOf(userpage) != -1) {
+        obj.data.year_vip_show = 1;
+        obj.data.level = 50;
+        obj.data.role = 1;
+        obj.data.is_vip = 1;
+        obj.data.level_icon = 'https://cdn.wtzw.com/bookimg/free/images/app/1_0_0/level/level_icon_50.png';
+    }
+
+    if (url.indexOf(level) != -1) {
+        obj.data.user.level = 50;
+        obj.data.user.year_vip_show = 1;
+        obj.data.user.exp = 401100;
+        obj.data.user.vip = 1;
+    }
+}
+
 $done({ body: JSON.stringify(obj) });
