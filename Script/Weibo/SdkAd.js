53163188343b52246b97936fbfed06ec73a9af15
翠花 <admin@nange.cn>
2022-06-24 23:55:41 +0800

    Update SdkAd.js

--- Script/Weibo/SdkAd.js
+++ Script/Weibo/SdkAd.js
@@ -1,3 +1,11 @@
 let body = JSON.parse($response.body.match(/({.*})OK/)[1]);  // $response.body： {json..,}OK
-body['ads'] = [];
+if (body.background_delay_display_time)
+    body.background_delay_display_time = 60*60*24*365;
+for (let item of body['ads']) {
+    // console.log(`${item['begintime']} -- ${item['endtime']}`);
+    item['displaytime'] = 0;  // 显示时间
+    // 2040 年
+    item['begintime'] = '2099-12-27 00:00:01';
+    item['endtime'] = '2099-12-27 23:59:59';
+}
 $done({body: `${JSON.stringify(body)}OK`});
