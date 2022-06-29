135b9a2d07d25129e14e10b8d7e4c76bc7dfa032
翠花 <admin@nange.cn>
2022-06-29 19:49:17 +0800

    Update.

--- Script/Tools/RewriteBody.js
+++ Script/Tools/RewriteBody.js
@@ -4,19 +4,22 @@
   如 Qx：
 https://service.ilovepdf.com/v1/user url response-body false response-body true
    
-可改写为 Surge：
-[Script]   
-test = type=http-response,pattern=https://service.ilovepdf.com/v1/user,requires-body=1,max-size=0,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js,argument=false=true
-   
-  argument=匹配值=替换值
-  支持正则：如argument=\w+=test
-  支持正则修饰符：如argument=/\w+/g=test
-  支持多参数，如：argument=匹配值1=替换值1&匹配值2=替换值2
-  
-  支持改写响应体和请求体体（type=http-response、request-body 注意必须打开需要body（requires-body=1）
-  
-  tips 修改json格式的键值对可以这样：
-  argument=("key"):\s?"(.+?)"=$1: "new_value"
+  可改写为 Surge：
+[Script] 
+test = type=http-response,pattern=https://service.ilovepdf.com/v1/user,requires-body=1,script-path=https://raw.githubusercontent.com/mieqq/mieqq/master/replace-body.js, argument=false->true
+
+argument=要匹配值=作为替换的值
+支持正则：如argument=\w+->test
+支持正则修饰符：如argument=/\w+/g->test
+支持多参数，如：argument=匹配值1->替换值1&匹配值2->替换值2
+
+支持改写响应体和请求体体（type=http-response 或 http-request）注意必须打开需要body（requires-body=1）
+
+tips 
+修改json格式的键值对可以这样：
+argument=("key"):\s?"(.+?)"->$1: "new_value"
+
+s修饰符可以让.匹配换行符，如 argument=/.+/s->hello
   
 */
 
@@ -42,7 +45,7 @@ if (typeof $argument == "undefined") {
 	}
 
 	$argument.split("&").forEach((item) => {
-		let [match, replace] = item.split("=");
+		let [match, replace] = item.split("->");
 		let re = getRegexp(match);
 		body = body.replace(re, replace);
 	});
