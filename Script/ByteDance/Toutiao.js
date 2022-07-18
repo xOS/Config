5906beeb6b6dbe56097085d003eb451c3964703f
翠花 <admin@nange.cn>
2022-07-19 04:01:09 +0800

    Update Toutiao.js

--- Script/ByteDance/Toutiao.js
+++ Script/ByteDance/Toutiao.js
@@ -10,8 +10,8 @@ if (body.data) {
             for (var i in data) {
                 let content = JSON.parse(data[i].content);
                 if (content.abstract == '' || content.card_title == '小视频' || content.video_source == 'ugc_video' || content.video_style == 2 || content.has_video == true) {
-                    //data[i] = {};
-                    delete data[i];
+                    data[i] = {};
+                    //delete data[i];
                 }
             }
         }
