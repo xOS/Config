81ef763d76a804f1ed91dc6d0a2f6c532e74bb87
翠花 <admin@nange.cn>
2023-01-26 23:06:10 +0800

    Update wnCalendar.js

--- Script/Tools/wnCalendar.js
+++ Script/Tools/wnCalendar.js
@@ -56,7 +56,7 @@ function doWork() {
                     notifyContent = '干支：' + i.gzYear + '年 ' + i.gzMonth + '月 ' + i.gzDate + '日\n农历：' + i.lMonth + '月' + i.lDate + '\n禁忌：' + i.avoid + '\n适宜：' + i.suit
                 }
             })
-            let title = '今日黄历' + desc
+            let title = '今日黄历' + ' | ' + desc
             $.isSurge() ? body = {
                 title: title,
                 content: notifyContent,
