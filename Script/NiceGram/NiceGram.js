7cde6430b58e69e020b56133bda020748df92237
翠花 <admin@nange.cn>
2023-12-06 00:30:54 +0800

    Update NiceGram.js with new rewrite rules

--- Script/NiceGram/NiceGram.js
+++ Script/NiceGram/NiceGram.js
@@ -1,5 +1,17 @@
-var obj = JSON.parse($response.body);
-obj['premium'] = true;
-obj['settings']['custom_notifications'] = true;
-obj['settings']['sync_chats'] = true;
-$done({ body: JSON.stringify(obj) });
+/*
+
+Nicegram 1.4.7
+
+[rewrite_local]
+https://nicegram.cloud/api/v6/user/info url script-response-body https://raw.githubusercontent.com/Yu9191/Rewrite/main/Nicegram.js
+
+[mitm] 
+hostname = nicegram.cloud
+
+**/
+
+var Q = JSON.parse($response.body);
+Q.data.user.lifetime_subscription = true;
+Q.data.user.store_subscription = true;
+Q.data.user.subscription = true;
+$done({body : JSON.stringify(Q)});
