c094c699428e3615192676ea1b8ef4b313253202
翠花 <admin@nange.cn>
2025-05-15 04:05:38 +0800

    Update

--- Script/Aloha/Aloha.js
+++
@@ -1,3 +0,0 @@
-var obj = JSON.parse($response.body);
-obj['profile']['is_premium'] = true;
-$done({ body: JSON.stringify(obj) });
--- Script/UptimeRobot/UptimeRobot.js
+++
@@ -1,7 +0,0 @@
-var body = JSON.parse($response.body);
-if (body.account) {
-    body.account.sms_credits = 9000;
-    body.account.payment_processor = true;
-    body.account.subscription_expiry_date = '2099-12-21';
-}
-$done({ body: JSON.stringify(body) });
