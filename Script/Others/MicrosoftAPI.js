bf3dda1dc699a074d9f8e3b87649103a86213df4
翠花 <git@nange.cn>
2025-06-21 14:21:46 +0800

    Update.

--- Script/Others/MicrosoftAPI.js
+++ Script/Others/MicrosoftAPI.js
@@ -4,4 +4,4 @@ body = $request.body;
 let obj = JSON.parse(body);
 obj["passwordCredential"]["endDateTime"] = "2999-10-11T07:19:47.610Z";
 body = JSON.stringify(obj); 
-$done({body});
+$done({ body });
